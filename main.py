c279883b8cbaa887111a3bd0a01b58570cde562e
ndumisour <167532084+ndumisour@users.noreply.github.com>
2024-04-19 13:50:10 +0200

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,7 @@
+def on_forever():
+    pass
+basic.forever(on_forever)
+
+def on_forever2():
+    pass
+basic.forever(on_forever2)
---
+++ main.ts
@@ -0,0 +1,7 @@
+let face = 0
+basic.forever(function () {
+    led.plot(0, 0)
+})
+basic.forever(function () {
+    face = 67
+})
